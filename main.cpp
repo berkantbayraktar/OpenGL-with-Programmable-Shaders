6a119b47cb4360f189970ccf3c06ce49e4a0bdf4
ilker <ilkeraycicek36@gmail.com>
2018-12-26 16:59:45 +0300

    Finish resizable,fullscreen window and changing height factor with keys

--- main.cpp
+++ main.cpp
@@ -23,21 +23,39 @@ GLuint texture_location;
 GLuint height_factor_location;
 GLuint normal_location;
 
+glm::mat4 normal_matrix;
 glm::vec3 camera_position;
+glm::vec3 camera_gaze;
+glm::mat4 view_matrix;
+glm::mat4 mvp;
+glm::mat4 mv;
+glm::mat4 model_matrix;
+glm::mat4 projection_matrix;
+
+GLfloat heightFactor;
 
 GLfloat*  buffer;
 
+bool isFullScreen = false;
+int window_width = 600;
+int window_height = 600;
 
 int widthTexture, heightTexture;
-float heightFactor = 10.0f;
 
-void setCamera();
+void init();
 void drawTriangles();
+void render();
+void setCamera();
+void key_callback(GLFWwindow* window, int key , int scancode , int action , int mods);
+void window_size_callback(GLFWwindow* window, int width, int height);
+void full_screen();
+
 
 static void errorCallback(int error,
   const char * description) {
   fprintf(stderr, "Error: %s\n", description);
 }
+
 
 int main(int argc, char * argv[]) {
   if (argc != 2) {
@@ -56,7 +74,7 @@ int main(int argc, char * argv[]) {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
   
 
-  win = glfwCreateWindow(600, 600, "CENG477 - HW3", NULL, NULL);
+  win = glfwCreateWindow(window_width, window_height, "CENG477 - HW3", NULL, NULL);
 
   if (!win) {
     glfwTerminate();
@@ -72,61 +90,24 @@ int main(int argc, char * argv[]) {
     exit(-1);
   }
 
+  glfwSetKeyCallback(win,key_callback);
+  glfwSetWindowSizeCallback(win, window_size_callback);
 
 
-  glEnable(GL_DEPTH_TEST);
-  glDepthFunc(GL_LESS);
-  glEnable(GL_CULL_FACE);
-  glCullFace(GL_BACK);
-  
   initShaders();
   glUseProgram(idProgramShader);
   initTexture(argv[1], & widthTexture, & heightTexture);
 
+  // initialize program
+  init();
 
-  normal_location = glGetUniformLocation(idProgramShader,"normalMatrix");
-  mvp_location = glGetUniformLocation(idProgramShader,"MVP");
-  idMVPMatrix = glGetUniformLocation(idProgramShader,"MV");
-  cam_pos_location = glGetUniformLocation(idProgramShader,"cameraPosition");
-  width_texture_location = glGetUniformLocation(idProgramShader,"widthTexture");
-  height_texture_location = glGetUniformLocation(idProgramShader,"heightTexture");
-  texture_location = glGetUniformLocation(idProgramShader,"rgbTexture");
-  height_factor_location = glGetUniformLocation(idProgramShader,"heightFactor");
 
- 
-  glUniform1i(texture_location,0);
-  glUniform1i(width_texture_location,widthTexture);
-  glUniform1i(height_texture_location,heightTexture);
-  glUniform1f(height_factor_location,heightFactor);
-
-  buffer = new GLfloat[12*widthTexture*heightTexture];
-  drawTriangles();
-  setCamera();
-
-  
-		
 
   // MAIN LOOP
   while (!glfwWindowShouldClose(win)) {
 
-    glViewport(0,0,600,600);
-        
-
-    // White background
-	  glClearColor(0,0,0,1);
-    glClearDepth(1.0f);
-    glClearStencil(0);
-    // Clear the screen
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    render();
     
-    setCamera();
-
-    // If doesn't work UNCOMMENT THESEE!!!
-    //glEnableClientState(GL_VERTEX_ARRAY);
-    //glVertexPointer(3,GL_FLOAT,0,buffer);
-		// Draw the triangle  
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4 * widthTexture * heightTexture); // 3 indices starting at 0 -> 1 triangle
-    //glDisableClientState(GL_VERTEX_ARRAY);
     
     glfwSwapBuffers(win);
     glfwPollEvents();
@@ -139,35 +120,46 @@ int main(int argc, char * argv[]) {
   return 0;
 }
 
-void setCamera()
-{ 
-  // Model matrix is identity , since there is no rotation translation etc.
-  glm::mat4 model_matrix = glm::mat4(1);
+void init()
+{
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+  glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
 
-  glm::mat4 projection_matrix  = glm::perspective(45.0f
-  , 1.0f , 0.1f, 1000.0f);
-
+  // height factor 10 initially
+  heightFactor = 10.0f;
+  // camera position
   camera_position = glm::vec3(widthTexture / 2, widthTexture / 10, (-1) * (widthTexture / 4));
-  
-  glm::mat4 view_matrix = glm::lookAt(
-    camera_position, // Camera position
-    glm::vec3(camera_position + glm::vec3(0,0,1)), // and looks to the z direction
-    glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-  ); 
+  //camera gaze
+  camera_gaze = glm::vec3(0,0,1);
+  // model matrix identity matrix initially
+  model_matrix = glm::mat4(1);
+  // perspective projection
+  projection_matrix  = glm::perspective(45.0f, 1.0f , 0.1f, 1000.0f);
 
-  glm::mat4 normal_matrix = glm::inverseTranspose(view_matrix);
-  
-  glm::mat4 mvp = projection_matrix * view_matrix * model_matrix;
-  
-  glm::mat4 mv = view_matrix * model_matrix;
-  
-  
-  glUniformMatrix4fv(mvp_location , 1 , GL_FALSE , &mvp[0][0]);
-  glUniformMatrix4fv(idMVPMatrix , 1 , GL_FALSE , &mv[0][0]);
-  glUniform3fv(cam_pos_location,1,&camera_position[0]);
-  glUniformMatrix4fv(normal_location,1,GL_FALSE,&normal_matrix[0][0]);
+  // 12 * w * h vertex count
+  buffer = new GLfloat[12*widthTexture*heightTexture];
 
-  
+  // insert triangles into buffer
+  drawTriangles();
+
+
+  // get variable locations from the shaders
+  normal_location = glGetUniformLocation(idProgramShader,"normalMatrix");
+  mvp_location = glGetUniformLocation(idProgramShader,"MVP");
+  idMVPMatrix = glGetUniformLocation(idProgramShader,"MV");
+  cam_pos_location = glGetUniformLocation(idProgramShader,"cameraPosition");
+  width_texture_location = glGetUniformLocation(idProgramShader,"widthTexture");
+  height_texture_location = glGetUniformLocation(idProgramShader,"heightTexture");
+  texture_location = glGetUniformLocation(idProgramShader,"rgbTexture");
+  height_factor_location = glGetUniformLocation(idProgramShader,"heightFactor");
+
+  // Insert permanent ones
+  glUniform1i(texture_location,0);
+  glUniform1i(width_texture_location,widthTexture);
+  glUniform1i(height_texture_location,heightTexture);
+
 }
 
 // Triangles clock-wise direction
@@ -217,3 +209,105 @@ void drawTriangles()
   delete[] buffer;
 		
 }
+
+void render()
+{ 
+        
+  //Black background
+	glClearColor(0,0,0,1);
+  glClearDepth(1.0f);
+  glClearStencil(0);
+
+  // Clear the screen
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+  setCamera();
+
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4 * widthTexture * heightTexture); // 3 indices starting at 0 -> 1 triangle
+  
+  
+  glUniform1f(height_factor_location,heightFactor);
+}
+
+void setCamera()
+{ 
+  
+  view_matrix = glm::lookAt(
+    camera_position, // Camera position
+    glm::vec3(camera_position + camera_gaze), // and looks to the z direction
+    glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+  ); 
+
+  normal_matrix = glm::inverseTranspose(view_matrix);
+  
+  mvp = projection_matrix * view_matrix * model_matrix;
+  
+  mv = view_matrix * model_matrix;
+  
+  
+  glUniformMatrix4fv(mvp_location , 1 , GL_FALSE , &mvp[0][0]);
+  glUniformMatrix4fv(idMVPMatrix , 1 , GL_FALSE , &mv[0][0]);
+  glUniform3fv(cam_pos_location,1,&camera_position[0]);
+  glUniformMatrix4fv(normal_location,1,GL_FALSE,&normal_matrix[0][0]);
+}
+
+
+void key_callback(GLFWwindow* window, int key , int scancode , int action , int mods)
+{ 
+  // If any key is not pressed or released
+  if(action != GLFW_RELEASE && action != GLFW_PRESS)
+  {
+    return;
+  }
+  // If a key is pressed
+  if(action == GLFW_PRESS)
+  {
+    switch(key)
+    { 
+      // O -> increase height factor
+      case GLFW_KEY_O:
+        heightFactor += 0.5f;
+        break;
+      // L -> decrease height factor
+      case GLFW_KEY_L:
+        heightFactor -= 0.5f;
+        break;
+      case GLFW_KEY_F:
+        full_screen();
+        break;
+      default:
+        break;
+    }
+  }
+  
+}
+
+void window_size_callback(GLFWwindow* window, int width, int height)
+{ 
+  int w,h;
+  glfwGetFramebufferSize(win , &w , &h);
+  glViewport(0 , 0 , w, h);
+}
+
+void full_screen()
+{ 
+  isFullScreen = !isFullScreen;
+  if(isFullScreen)
+  { 
+    // Store old width and height
+    glfwGetWindowSize(win , &window_width , &window_height);
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+    glfwSetWindowMonitor(win , monitor, 0 , 0,
+    mode -> width , mode -> height , mode -> refreshRate);
+  }
+  else
+  {
+    glfwSetWindowMonitor(win , NULL , 0 , 0,
+    window_width, window_height, 0);
+  }
+  
+ 
+}
+
+
